e60daf1bec87559405958f5f0dd9cf31ddeb2268
antogeo <Georgios.Antonopoulos@ulg.ac.be>
2016-07-19 13:54:51 +0200

    Add files via upload

---
+++ calc_stats_on_seq.m
@@ -0,0 +1,31 @@
+% %%% make sequences of n featuers
+function [res] = calc_stats_on_seq(matrix_data, numofchar) 
+[subj_no, feat_no] = size(matrix_data);
+% Letter pattern kai arithmos emfaniseon
+pat_inf{1, 2} = 1; pat_inf{1, 1} = ''; line = 1;
+
+results = col2let(matrix_data);
+
+    for i = 1 : subj_no
+        ne{i, 1} = strvcat(results(i, :));
+        ne{i, 1} = strrep(ne{i}','0','');
+        
+    end
+    for no_subj = 1 : subj_no
+        for char_no = 1 : size(ne{no_subj}, 2) - (numofchar - 1)
+            char_pattern = ne{no_subj}(char_no: char_no + (numofchar - 1));
+            [start_ind, end_ind] = regexp(ne, char_pattern, 'match');
+            plithos_emf = size(cell2mat(end_ind), 1);
+            
+             if plithos_emf > 1
+                 if isempty(strmatch(char_pattern, pat_inf(:, 1)))
+                     pat_inf{line, 2} = plithos_emf;
+                     pat_inf{line, 1} = char_pattern;
+                     line = line + 1;
+                 end
+             end
+            fprintf('%s\n', char_pattern)
+       end
+    end
+res = pat_inf;    
+end
